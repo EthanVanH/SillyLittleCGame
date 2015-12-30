a86d9e7dcf9d5e8569e1645c53cc2064868406ea
Ethan <evanhout@uoguelph.ca>
2015-12-30 13:36:16 -0500

    added a structure for defining the player character

--- asset/gameStructures.h
+++ asset/gameStructures.h
@@ -10,6 +10,17 @@ typedef struct Invader
 	int health;
 	char image;
 } Invader;
+/*This struct defines the player character, it is much the same as the invader with the added variables
+* score, which keeps track of the number of invaders destroyed, nad
+* power, which is a modifiable attribute which defines how much a player does.
+*/
+typedef struct Player
+{
+	int health;
+	int power;
+	int score;
+	char image;
+};
 
 /*This is a simple List structure of invaders used to print to screen one at a time taken from our data structure library*/
 /*Only change is definition of item, which is now defined as an INvader*/
